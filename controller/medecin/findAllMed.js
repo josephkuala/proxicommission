13d11aeadf35ad7f3eb0fb41ee04e51616613bb6
Predator <josephkuala@gmail.com>
2024-09-11 13:23:31 +0100

    Update FindAllMed by adding typeuser object

--- controller/medecin/findAllMed.js
+++ controller/medecin/findAllMed.js
@@ -1,8 +1,18 @@
 const Medecin = require('../../models/medecin');
+const TypeUser = require('../../models/typeuser');
 
 exports.findAllMedecin = async (req, res) => {
    await Medecin.findAll(
-      { order: ['nom'] }
+      { order: ['nom'],
+        attributes: { exclude: ['typeuser'] },
+        include: [
+          {
+            model: TypeUser,
+            attributes: ['nom', 'pourcentage'] // Spécifiez les attributs que vous souhaitez du médecin
+          },
+        ]
+      }
+      
     )
     .then(medecins => {
       const message = 'La liste des medecins a bien été récupéré.'
